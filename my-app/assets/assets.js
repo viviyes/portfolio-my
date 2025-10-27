a8e275d3e045e76ec0e6140faf4f6f9f1c82cc03
Yuwei <yuwei163h@163.com>
2025-10-27 23:39:47 +1100

    update:assets_parts

---
+++ my-app/assets/assets.js
@@ -0,0 +1,263 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import github_icon from './github-icon.png';
+import phone_icon from './phone-icon.png';
+import linkedin from './linkedin.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import docker from './docker.png';
+import pycharm from './pycharm.png';
+import postman from './postman.png';
+import visual_studio from './visual-studio.png';
+import azure from './azure-logo.png';
+import mysql from './mysql.png';
+import html5 from './htm-logo.png';
+import css3 from './cs-logo.png';
+import javascript from './js-logo.png';
+import tailwindCSS from './tailwindcss-logo.png';
+import typescript from './typescript-logo.png';
+import react from './react-logo.png';
+import redux from './redux-logo.png';
+import nextjs from './nextjs-logo.png';
+import node from './nodejs-logo.png';
+import express from './express-logo.png';
+import sass from './sass-logo.png';
+import jquery from './jQuery-logo.png';
+import vue from './vue-logo.png';
+import nuxt from './nuxt-logo.png';
+import csharp from './csharp-logo.png';
+import dotnet from './dotnet-logo.png';
+import grapgql from './graphql-logo.png';
+import restapi from './restapi-logo.png';
+import php from './php-logo.png';
+import python from './python-logo.png';
+import postgresql from './postgresql-logo.png';
+import mongodblogo from './mongodb-logo.png';
+import sqlitlogo from './sqlite-logo.png';
+import awsS3 from './awsS3-logo.png';
+import cloudFrontlogo from './cloudFront-logo.png';
+import cloudFlarelogo from './cloudflare-logo.png';
+import ec2logo from './ec2-logo.png';
+import rdslogo from './rds-logo.png';
+import nginxlogo from './nginx-logo.png';
+import pslogo from './ps-logo.png';
+import clickLogo from './click-logo.png';
+import clickDarkLogo from './click-dark-logo.png';
+
+
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    github_icon,
+    phone_icon,
+    linkedin,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    docker,
+    pycharm,
+    postman,
+    visual_studio,
+    azure,
+    mysql,
+    html5,
+    css3,
+    javascript,
+    tailwindCSS,
+    typescript,
+    react,
+    redux,
+    nextjs,
+    node,
+    express,
+    sass,
+    jquery,
+    vue,
+    nuxt,
+    csharp,
+    dotnet,
+    grapgql,
+    restapi,
+    php,
+    python,
+    postgresql,
+    mongodblogo,
+    sqlitlogo,
+    awsS3,
+    cloudFrontlogo,
+    cloudFlarelogo,
+    ec2logo,
+    rdslogo,
+    nginxlogo,
+    pslogo,
+    clickLogo,
+    clickDarkLogo,
+};
+
+export const workData = [
+    {
+        title: 'Full-stack AI interview assistant',
+        description: ['Google Gemini', 'openai', 'MERN Stack', 'mongodb', 'express', 'React', 'nodejs', 'tailwind', 'JWT', 'bcryptjs'],
+        bgImage: '/work_ai_pic.png',
+        link: 'https://interviewai.vivicoding.com',
+    },
+    {
+        title: 'Full-stack Inventory Management',
+        description: ['React', 'Next', 'TypeScript', 'Tailwind', 'Node', 'Express', 'PostgreSQL', 'AWS'],
+        bgImage: '/work_manag_pic.png',
+        link: 'https://www.baidu.com',
+    },
+    {
+        title: 'Frontend Smart Park System',
+        description: ['React', 'TypeScript', 'react-router', 'Redux', 'Antd', 'Saas', 'Mock.js'],
+        bgImage: '/work_smartPark_pic.png',
+        link: 'https://www.baidu.com',
+    },
+    {
+        title: 'Frontend Amazon clone',
+        description: ['HTML', 'CSS', 'JavaScript'],
+        bgImage: '/work_amazon_pic2.png',
+        link: 'https://www.baidu.com',
+    },
+
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
+    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
+    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
+    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Tech Stack', description: 'HTML, CSS, tailwind, JavaScript, ES6, TypeScript, React, Redux, Vue, Next, Node, Express, C#, .NET, PostgreSQL, MySQL, MongoDB, AWS' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Master of Information Technology, UNSW' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsTabs = [
+    { id: "Frontend", label: "Frontend" },
+    { id: "Backend", label: "Backend" },
+    { id: "Database", label: "Database" },
+    { id: "Cloud", label: "Cloud" },
+    { id: "Tools", label: "Tools" },
+]
+
+export const frontendStack = [
+    { icon: assets.react, name: 'React', des: 'JS Framework' },
+    { icon: assets.nextjs, name: 'Next.js', des: 'React Framework' },
+    { icon: assets.redux, name: 'Redux', des: 'State Management' },
+    { icon: assets.typescript, name: 'TypeScript', des: 'Type Safety' },
+    { icon: assets.vue, name: 'Vue', des: 'JS Framework' },
+    { icon: assets.nuxt, name: 'Nuxt', des: 'Vue Framework' },
+    { icon: assets.javascript, name: 'JavaScript', des: 'Interaction' },
+    { icon: assets.jquery, name: 'jQuery', des: 'JS Library' },
+    { icon: assets.html5, name: 'HTML5', des: 'Markup Language' },
+    { icon: assets.css3, name: 'CSS3', des: 'User Interface' },
+    { icon: assets.sass, name: 'Sass', des: 'CSS Preprocessor' },
+    { icon: assets.tailwindCSS, name: 'TailwindCSS', des: 'User Interface' },
+];
+
+
+export const backendStack = [
+    { icon: assets.csharp, name: 'CSharp', des: 'Backend Language' },
+    { icon: assets.dotnet, name: '.NET', des: 'Backend Framework' },
+    { icon: assets.node, name: 'Node.js', des: 'Web Server' },
+    { icon: assets.express, name: 'Express', des: 'Node Framework' },
+    { icon: assets.grapgql, name: 'GraphQL', des: 'API' },
+    { icon: assets.restapi, name: 'REST API', des: 'API' },
+    { icon: assets.php, name: 'PHP', des: 'Backend Language' },
+    { icon: assets.python, name: 'Python', des: 'Backend Language' },
+];
+
+export const databaseStack = [
+    { icon: assets.mongodblogo, name: 'MongoDB', des: 'Non-relational' },
+    { icon: assets.mysql, name: 'MySQL', des: 'Relational (RDS)' },
+    { icon: assets.postgresql, name: 'PostgreSQL', des: 'Relational (RDS)' },
+    { icon: assets.sqlitlogo, name: 'SQLite', des: 'Relational (RDS)' },
+];
+
+export const cloudStack = [
+    { icon: assets.awsS3, name: 'AWS S3', des: 'Cloud Storage' },
+    { icon: assets.cloudFrontlogo, name: 'CloudFront', des: 'CDN' },
+    { icon: assets.cloudFlarelogo, name: 'CloudFlare', des: 'CDN' },
+    { icon: assets.ec2logo, name: 'EC2', des: 'Cloud Computing' },
+    { icon: assets.rdslogo, name: 'RDS', des: 'Cloud Computing' },
+    { icon: assets.nginxlogo, name: 'Nginx', des: 'Web Server' },
+]
+
+export const toolsStack = [
+    { icon: assets.vscode, name: 'VS Code', des: 'Code Editor' },
+    { icon: assets.firebase, name: 'Firebase', des: 'Platform' },
+    { icon: assets.git, name: 'Git', des: 'Version Control' },
+    { icon: assets.docker, name: 'Docker', des: 'Containerization' },
+    { icon: assets.postman, name: 'Postman', des: 'API Testing' },
+    { icon: assets.pslogo, name: 'Photoshop', des: 'Image Editing' },
+]
+
